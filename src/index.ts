47a86c2fc4046ad1c12ea546be9ff3ef91bb6937
woochul2 <leewoo0686@gmail.com>
2021-01-12 00:17:57 +0900

    feat: Apollo Server 설정

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import { MikroORM } from '@mikro-orm/core';
 import mikroConfig from './mikro-orm.config';
-// import { Post } from './entities/Post';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, { title: 'Test' });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('서버가 시작됐습니다.');
+    console.log('이제 브라우저에서 Apollo Server를 볼 수 있습니다.');
+    console.log('로컬 주소: http://localhost:4000/graphql');
+  });
 };
 
 main().catch((err) => {
